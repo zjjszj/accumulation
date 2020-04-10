9daa97505b3f38e9140458ec97ffbf63f67d4e1a
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2020-04-10 18:29:45 +0800

    Initial commit

--- resnet50_fpn.py
+++ resnet50_fpn.py
@@ -77,7 +77,10 @@ preprocess = T.Compose([
 )
 img=preprocess(img)
 img=img.reshape(1,3,448,448)   #torch.float32
-plt.imshow(img.reshape(3,448,448).data.numpy()[0,:,:])
+#显示图像
+img_show=img.reshape(3,448,448).data.numpy()
+img_show=np.transpose(img_show,(1,2,0))
+plt.imshow(img_show)
 plt.show()
 
 
@@ -98,9 +101,9 @@ def testFPN():
     inputs=img
     [p2_out, p3_out, p4_out, p5_out] = resnet50_fpn(inputs)  #/4  /8 /16 /32
 
-    plt.imshow(p2_out.reshape(256, 112, 112).data.numpy()[0, :, :])
-    plt.show()
-
+    p2_out_show=p2_out.reshape(256, 112, 112)
+    p2_out_show=T.ToPILImage()(p2_out_show)
+    p2_out_show.show()
 
 if __name__=='__main__':
     testFPN()
