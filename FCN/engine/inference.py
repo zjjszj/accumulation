2f2e08bc953eebbc08b8bcf4c1c31e9bde8b7bca
zhang jin <hblfgazj@163.com>
2020-08-10 18:46:48 +0800

    fixed bug

--- FCN/engine/inference.py
+++ FCN/engine/inference.py
@@ -18,25 +18,25 @@ class Inference:
 
         mloss=torch.zeros(1)
         eval_loss,  eval_acc, eval_acc_cls, eval_mean_iu, eval_fwavacc= 0, 0, 0, 0, 0
+        with torch.no_grad:
+            pbar=tqdm(enumerate(self.loader), total=len(self.loader))
+            for i, data in pbar:
+                imgs, label=data
+                imgs, label=imgs.to(self.device), label.to(self.device)
+                out=self.model(imgs)
+                loss=self.criterion(out, label)
+                mloss=(mloss*i+loss)/(i+1)
 
-        pbar=tqdm(enumerate(self.loader), total=len(self.loader))
-        for i, data in pbar:
-            imgs, label=data
-            imgs, label=imgs.to(self.device), label.to(self.device)
-            out=self.model(imgs)
-            loss=self.criterion(out, label)
-            mloss=(mloss*i+loss)/(i+1)
+                label_pred = out.max(dim=1)[1].data.cpu().numpy()
+                label_true = label.data.cpu().numpy()
+                for lbt, lbp in zip(label_true, label_pred):
+                    acc, acc_cls, mean_iu, fwavacc = label_accuracy_score(lbt, lbp, self.cfg.MODEL.NUM_CLASSES )
+                    eval_acc += acc
+                    eval_acc_cls += acc_cls
+                    eval_mean_iu += mean_iu
+                    eval_fwavacc += fwavacc
 
-            label_pred = out.max(dim=1)[1].data.cpu().numpy()
-            label_true = label.data.cpu().numpy()
-            for lbt, lbp in zip(label_true, label_pred):
-                acc, acc_cls, mean_iu, fwavacc = label_accuracy_score(lbt, lbp, self.cfg.MODEL.NUM_CLASSES )
-                eval_acc += acc
-                eval_acc_cls += acc_cls
-                eval_mean_iu += mean_iu
-                eval_fwavacc += fwavacc
-
-            s=('loss=%.5g\teval_acc=%5.g\teval_acc_cls=%5.g\teval_mean_iu=%5.g\teval_fwavacc=%5.g')%(loss, eval_acc, eval_acc_cls, eval_mean_iu, eval_fwavacc)
-            pbar.set_description(s)
+                s=('loss=%.5g\teval_acc=%5.g\teval_acc_cls=%5.g\teval_mean_iu=%5.g\teval_fwavacc=%5.g')%(loss, eval_acc, eval_acc_cls, eval_mean_iu, eval_fwavacc)
+                pbar.set_description(s)
         return mloss, eval_acc, eval_acc_cls, eval_mean_iu, eval_fwavacc
 
